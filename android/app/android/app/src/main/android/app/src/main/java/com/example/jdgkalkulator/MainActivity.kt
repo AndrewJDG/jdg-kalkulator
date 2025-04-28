db5d4169492dfb98aa46d3b049310159bd4654b1
AndrewJDG <renault9218@gmail.com>
2025-04-28 20:30:32 +0200

    Create MainActivity.kt

---
+++ android/app/android/app/src/main/android/app/src/main/java/com/example/jdgkalkulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jdggkalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Здесь будет твой основной код
+    }
+}
